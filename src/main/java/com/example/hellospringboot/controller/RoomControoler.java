45bbb75a3d72feb22228f1459e4c9baa3a896282
haoshuai <haoshuai9192@gmail.com>
2023-04-01 09:57:24 +0800

    更新配置文件

---
+++ src/main/java/com/example/hellospringboot/controller/RoomControoler.java
@@ -0,0 +1,11 @@
+package com.example.hellospringboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/room")
+public class RoomControoler {
+
+}
